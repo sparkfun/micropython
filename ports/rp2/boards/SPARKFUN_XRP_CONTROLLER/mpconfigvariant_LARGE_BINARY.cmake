7e728e8c6aad74ca244183f3e0705db6f332abd9
Dryw Wade <dryw.wade@sparkfun.com>
2025-07-10 13:12:51 -0600

    Add large binary variant of XRP

---
+++ ports/rp2/boards/SPARKFUN_XRP_CONTROLLER/mpconfigvariant_LARGE_BINARY.cmake
@@ -0,0 +1,6 @@
+list(APPEND MICROPY_DEF_BOARD
+    # Board name
+    "MICROPY_HW_BOARD_NAME=\"SparkFun XRP Controller (Large Binary)\""
+    # 8MB (8 * 1024 * 1024)
+    "MICROPY_HW_FLASH_STORAGE_BYTES=8388608"
+)
