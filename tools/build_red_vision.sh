ac6bef50377b24ab55cac6363ea37ee79b3bf008
Dryw Wade <dryw.wade@sparkfun.com>
2025-08-01 18:54:12 -0600

    Fix Red Vision build script Hopefully...

--- tools/build_red_vision.sh
+++ tools/build_red_vision.sh
@@ -13,28 +13,30 @@ function build_micropython_red_vision {
     # Install necessary python packages (could also move this to a requirements.txt file)
     pip install freezefs
 
-    # Clone the Red Vision submodule
-    git submodule update --init --recursive micropython/lib/red_vision
-
     # Set Pico SDK path to $GITHUB_WORKSPACE/micropython/lib/pico-sdk if $GITHUB_WORKSPACE is set, otherwise use the current directory
     if [ -n "$GITHUB_WORKSPACE" ]; then
         export PICO_SDK_PATH="$GITHUB_WORKSPACE/micropython/lib/pico-sdk"
+        # Ensure we're in the micropython directory
+        cd micropython
     else
-        export PICO_SDK_PATH=$(dirname "$0")/micropython/lib/pico-sdk
+        export PICO_SDK_PATH=$(dirname "$0")/lib/pico-sdk
     fi
 
+    # Clone the Red Vision submodule
+    git submodule update --init --recursive lib/red_vision
+
     # Build MPY Cross compiler
-    make -C micropython/mpy-cross ${MAKEOPTS}
+    make -C mpy-cross ${MAKEOPTS}
 
     # Update necessary MicroPython submodules
-    make -C micropython/ports/rp2 BOARD=SPARKFUN_XRP_CONTROLLER submodules
+    make -C ports/rp2 BOARD=SPARKFUN_XRP_CONTROLLER submodules
 
     # Build OpenCV
-    make -C micropython/lib/red_vision/micropython-opencv PLATFORM=rp2350 --no-print-directory ${MAKEOPTS}
+    make -C lib/red_vision/micropython-opencv PLATFORM=rp2350 --no-print-directory ${MAKEOPTS}
 
     # Build firmware
-    make -C micropython/lib/red_vision PORT_DIR=micropython/ports/rp2 BOARD=SPARKFUN_XRP_CONTROLLER ${MAKEOPTS}
+    make -C lib/red_vision PORT_DIR=~/micropython/ports/rp2 BOARD=SPARKFUN_XRP_CONTROLLER --no-print-directory ${MAKEOPTS}
 
     # Rename firmware file to identify it as the Red Vision build and which board it's for
-    mv micropython/ports/rp2/build-SPARKFUN_XRP_CONTROLLER-RED_VISION/firmware.uf2 micropython/ports/rp2/build-SPARKFUN_XRP_CONTROLLER-RED_VISION/RED_VISION_MICROPYTHON_SPARKFUN_XRP_CONTROLLER.uf2
+    mv ports/rp2/build-SPARKFUN_XRP_CONTROLLER-RED_VISION/firmware.uf2 ports/rp2/build-SPARKFUN_XRP_CONTROLLER-RED_VISION/RED_VISION_MICROPYTHON_SPARKFUN_XRP_CONTROLLER.uf2
 }
