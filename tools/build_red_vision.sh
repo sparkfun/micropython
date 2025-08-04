602f64ad05a3c2135d2f5b49355c7ec76834b4ef
Dryw Wade <dryw.wade@sparkfun.com>
2025-08-04 16:16:20 -0600

    Remove Makefile from Red Vision Update build script with the relevant Make and Cmake arguments Also fix PICO_SDK_PATH for local builds

--- tools/build_red_vision.sh
+++ tools/build_red_vision.sh
@@ -14,10 +14,12 @@ function build_micropython_red_vision {
     pip install freezefs
 
     # Set Pico SDK path to $GITHUB_WORKSPACE/micropython/lib/pico-sdk if $GITHUB_WORKSPACE is set, otherwise use the current directory
+    # https://stackoverflow.com/a/246128/4783963
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     if [ -n "$GITHUB_WORKSPACE" ]; then
         export PICO_SDK_PATH="$GITHUB_WORKSPACE/micropython/lib/pico-sdk"
     else
-        export PICO_SDK_PATH=$(dirname "$0")/lib/pico-sdk
+        export PICO_SDK_PATH="$SCRIPT_DIR/../lib/pico-sdk"
     fi
 
     # Clone the Red Vision submodule
@@ -27,7 +29,10 @@ function build_micropython_red_vision {
     make -C mpy-cross ${MAKEOPTS}
 
     # Update necessary MicroPython submodules
-    make -C ports/rp2 BOARD=SPARKFUN_XRP_CONTROLLER submodules
+    make -C ports/rp2 \
+        BOARD=SPARKFUN_XRP_CONTROLLER \
+        BOARD_VARIANT=RED_VISION \
+        submodules
 
     # Build OpenCV
     make -C lib/red_vision/micropython-opencv PLATFORM=rp2350 --no-print-directory ${MAKEOPTS}
@@ -35,8 +40,20 @@ function build_micropython_red_vision {
     # Archive the examples directory
     python3 -m freezefs lib/red_vision/red_vision_examples lib/red_vision/extract_red_vision_examples.py  --on-import=extract --compress --overwrite always
 
+    # Set CMake arguments for Pico SDK to use MicroPython-OpenCV malloc wrappers
+    # and enable C++ exceptions
+    export CMAKE_ARGS="-DSKIP_PICO_MALLOC=1 -DPICO_CXX_ENABLE_EXCEPTIONS=1"
+
     # Build firmware
-    make -C lib/red_vision PORT_DIR=~/micropython/ports/rp2 BOARD=SPARKFUN_XRP_CONTROLLER --no-print-directory ${MAKEOPTS}
+    make -C ports/rp2 \
+        BOARD=SPARKFUN_XRP_CONTROLLER \
+        BOARD_VARIANT=RED_VISION \
+        USER_C_MODULES="$SCRIPT_DIR/../lib/red_vision/micropython-opencv/micropython_opencv.cmake" \
+        FROZEN_MANIFEST="$SCRIPT_DIR/../lib/red_vision/manifest.py" \
+        --no-print-directory ${MAKEOPTS}
+    
+    # Unset CMake arguments to avoid affecting other builds
+    unset CMAKE_ARGS
 
     # Rename firmware file to identify it as the Red Vision build and which board it's for
     mv ports/rp2/build-SPARKFUN_XRP_CONTROLLER-RED_VISION/firmware.uf2 ports/rp2/build-SPARKFUN_XRP_CONTROLLER-RED_VISION/RED_VISION_MICROPYTHON_SPARKFUN_XRP_CONTROLLER.uf2
